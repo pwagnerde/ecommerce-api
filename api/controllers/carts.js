cb284f13276f373b8792fac6ed52f20537c369f9
Philipp W <74567206+pwagnerde@users.noreply.github.com>
2021-02-19 23:07:06 +0100

    #8 Shopping cart checkout

--- api/controllers/carts.js
+++ api/controllers/carts.js
@@ -3,7 +3,12 @@
 const db = require("../../db");
 
 // Exports all the functions to perform on the db
-module.exports = { getCartsAll, getCartsCustomer, getCartItems };
+module.exports = {
+  getCartsAll,
+  getCartsCustomer,
+  getCartItems,
+  orderCartItems,
+};
 
 //GET /carts operationId
 async function getCartsAll(req, res, next) {
@@ -14,7 +19,9 @@ async function getCartsAll(req, res, next) {
     if (rows[0]) {
       res.status(200).json({ orders: rows });
     } else {
-      res.status(204).json({ message: `No shopping cart data found in database` });
+      res
+        .status(204)
+        .json({ message: `No shopping cart data found in database` });
     }
   } catch (e) {
     res.status(500).json({ message: e });
@@ -34,7 +41,9 @@ async function getCartsCustomer(req, res, next) {
     if (rows[0]) {
       res.status(200).json({ orders: rows });
     } else {
-      res.status(204).json({ message: `No shopping cart data found in database` });
+      res
+        .status(204)
+        .json({ message: `No shopping cart data found in database` });
     }
   } catch (e) {
     res.status(500).json({ message: e });
@@ -64,13 +73,61 @@ async function getCartItems(req, res, next) {
 }
 
 //POST /carts/{cartId}/items?product_id={id} operationId
-async function addCartItems(req, res, next) {
-}
+async function addCartItems(req, res, next) {}
 
 //PUT /carts/{cartId}/items?product_id={id} operationId
-async function updateCartItems(req, res, next) {
-}
+async function updateCartItems(req, res, next) {}
 
 //DELETE /carts/{cartId}/items?product_id={id} operationId
-async function removeCartItems(req, res, next) {
+async function removeCartItems(req, res, next) {}
+
+//POST /carts/{cartId}/checkout
+async function orderCartItems(req, res, next) {
+  const cartId = req.swagger.params.cart_id.value; //req.swagger contains the path parameters
+  const today = new Date();
+  const created_at = today.toISOString().substring(0, 10);
+  const completedOrderStatus = 6; //Completed
+  const orderedShoppingCardStatus = 3; //checked out
+  const newShoppingCardStatus = 1; //open
+  try {
+    await db.query("BEGIN");
+    const {
+      rows,
+    } = await db.query(
+      "INSERT INTO public.order (customer_id, status_code_id, created_at) VALUES((SELECT customer_id from public.shopping_cart WHERE id = $1),$2,$3) RETURNING id",
+      [cartId, completedOrderStatus, created_at]
+    );
+    const newOrderId = rows[0].id;
+    const result = await db.query(
+      "SELECT product_id, quantity, price FROM shopping_cart_item WHERE shopping_cart_id = $1",
+      [cartId]
+    );
+    const cart_items = result.rows;
+    for (let i = 0; i < cart_items.length; i++) {
+      await db.query(
+        "INSERT INTO public.order_item (order_id, product_id, quantity, price) VALUES($1,$2,$3,$4)",
+        [
+          newOrderId,
+          cart_items[i].product_id,
+          cart_items[i].quantity,
+          cart_items[i].price,
+        ]
+      );
+    }
+    await db.query(
+      "UPDATE public.shopping_cart SET status_code_id = $1 WHERE id = $2",
+      [orderedShoppingCardStatus, cartId]
+    );
+    await db.query(
+      "INSERT INTO public.shopping_cart (customer_id, status_code_id, created_at) VALUES((SELECT customer_id from public.shopping_cart WHERE id = $1),$2,$3)",
+      [cartId, newShoppingCardStatus, created_at]
+    );
+    await db.query("COMMIT");
+    res
+      .status(200)
+      .json({ message: `Shopping card ordered. Order id: ${newOrderId}` });
+  } catch (e) {
+    console.log("CATCH");
+    res.status(500).json({ message: e });
+  }
 }
